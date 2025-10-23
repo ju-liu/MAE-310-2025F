898831f8ddce06bacf8d350e62e202d49b432361
Ju Liu <liujuy@gmail.com>
2025-10-23 20:53:07 +0800

    in

---
+++ codes/heat1D/driver.m
@@ -0,0 +1,51 @@
+clear all; clc;
+
+% -----------------------------------------------
+% material parameters and input da
+% define the manufactured solution
+exact   = @(x) sin(x);
+exact_x = @(x) cos(x);
+
+% define the input for the differential equation
+f = @(x) sin(x);
+g = sin(1);
+h = -1;
+
+% define the discretization parameters
+n_el = 5;         % number of elements
+n_en = 2;         % number of element nodes
+n_np = n_el + 1;  % number of nodal points
+hh   = 1 / n_el;  % uniform element length
+
+x_coor = 0 : hh : 1; % nodal coordinates
+
+% Data structures
+IEN = zeros(n_en, n_el);
+
+for ee = 1 : n_el
+  for aa = 1 : n_en
+    IEN(aa, ee) = ee - 1 + aa;
+  end
+end
+
+ID = 1 : n_np;
+ID(end) = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% EOF
